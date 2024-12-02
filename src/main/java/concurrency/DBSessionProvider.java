ba4d821bf418633a83e059d807894de3fb5831c6
Julien-Hanin <158847625+Julien-Hanin@users.noreply.github.com>
2024-12-02 13:48:39 +0100

    Create DBSessionProvider.java

---
+++ src/main/java/concurrency/DBSessionProvider.java
@@ -0,0 +1,33 @@
+package concurrency;
+
+import org.eclipse.persistence.sessions.Session;
+import org.eclipse.persistence.sessions.server.ServerSession;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class DBSessionProvider {
+
+  private static DBSessionProvider singleton = null;
+
+  public static synchronized DBSessionProvider getInstance() {
+    if (singleton == null) {
+      singleton = new DBSessionProvider();
+    }
+    return singleton;
+  }
+
+  private final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("testJPA");
+
+  protected ServerSession getServerSession() {
+    return entityManagerFactory.unwrap(ServerSession.class);
+  }
+
+  public Session getNewClientSession() {
+    return getServerSession().acquireClientSession();
+  }
+
+  public EntityManagerFactory getEntityManagerFactory() {
+    return entityManagerFactory;
+  }
+}
